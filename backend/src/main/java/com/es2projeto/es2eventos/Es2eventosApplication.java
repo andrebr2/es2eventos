b13c303d41ba4f680c1c4092c64183c86407b0a9
andrebr2 <andrebrandao12@yahoo.com>
2025-10-08 21:59:56 -0300

    Projeto criado

---
+++ backend/src/main/java/com/es2projeto/es2eventos/Es2eventosApplication.java
@@ -0,0 +1,13 @@
+package com.es2projeto.es2eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Es2eventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Es2eventosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/es2projeto/es2eventos/Es2eventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.es2projeto.es2eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Es2eventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
